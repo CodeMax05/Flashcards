5d9d35bc02f286711ac46a59a999f0aee190c15a
David Loja <d4vd.005@gmail.com>
2026-03-08 18:18:06 -0400

    Setup basic structure for website and some hook functions - not fully implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCardCount] = useState(10);
+  const [flipped, setFlipCards] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const updateCardCount = () => setCardCount(cards - flipped);
+  
+  return(
+    <div>
+      <h2>The Ultimate Korean Learner</h2>
+      <h4>Lets test to see how much korean you know!</h4>
+      <h5>Number of Cards: {cards}</h5>
+      <div className='Cards'>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button onClick={updateCardCount}>Next</button>
+    </div>
   )
 }
 
