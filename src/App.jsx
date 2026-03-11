bcdbb19f5d45a0f794ceeabd8048b4a14b8818e9
David Loja <d4vd.005@gmail.com>
2026-03-10 23:05:36 -0400

    Made the card flip css to work alongside the card component while also adding a background to the page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ function App() {
     { question: 'How do you say hello?', answer: '안녕하세요' },
     { question: 'How do you say goodbye?', answer: '안녕히 가세요' },
     { question: 'What does 주황색 mean?', answer: 'orange' },
+    { question: 'How do you say thank you?', answer: '감사합니다' },
+    { question: 'How do you say yes?', answer: '네' },
+    { question: 'How do you say no?', answer: '아니요' },
+    { question: 'What does 물 mean?', answer: 'water' },
+    { question: 'How do you say I love you?', answer: '사랑해요' },
+    { question: 'What does 밥 mean?', answer: 'rice / meal' },
+    { question: 'How do you say sorry?', answer: '죄송합니다' },
   ];
 
   const handleFlip = () => {
@@ -25,6 +32,7 @@ function App() {
 
   return(
     <div>
+      <img className='background' src="https://m.media-amazon.com/images/I/81di25RIlhL.jpg" alt="" />
       <h2>The Ultimate Korean Learner</h2>
       <h4>Let's test to see how much Korean you know!</h4>
       <h5>Card {currentIndex + 1} of {cards.length}</h5>
