28241f7db2754281f3d247b101b65713c637ee1a
David Loja <d4vd.005@gmail.com>
2026-03-10 22:55:25 -0400

    Adjusted the Card component to work with the flip function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
-const Card = (props) => {
+const Card = ({ question, answer, isFlipped, onClick }) => {
     return(
-        <div className="Card">
-            <h4>{props.response}</h4>
+        <div className="card-container" onClick={onClick}>
+            <div className={`card-inner ${isFlipped ? 'flipped' : ''}`}>
+                <div className="card-front">
+                    <h4>{question}</h4>
+                </div>
+                <div className="card-back">
+                    <h4>{answer}</h4>
+                </div>
+            </div>
         </div>
     )
 }
