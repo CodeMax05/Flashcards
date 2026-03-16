8a3d5780230ebbfc86e38059c48460ac05a4e3e8
David Loja <d4vd.005@gmail.com>
2026-03-16 00:53:29 -0500

    Added the previous and next buttons to control which card users see as they practice. Also added the shuffle button to mix the cards moving the card list into a state so that they can be altered while also making the new index random.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import './App.css'
 function App() {
   const [isFlipped, setIsFlipped] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  const cards = [
+  const [cards, setCards] = useState([
     { question: 'How do you say hello?', answer: '안녕하세요' },
     { question: 'How do you say goodbye?', answer: '안녕히 가세요' },
     { question: 'What does 주황색 mean?', answer: 'orange' },
@@ -18,7 +17,7 @@ function App() {
     { question: 'How do you say I love you?', answer: '사랑해요' },
     { question: 'What does 밥 mean?', answer: 'rice / meal' },
     { question: 'How do you say sorry?', answer: '죄송합니다' },
-  ];
+  ]);
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
@@ -28,6 +27,27 @@ function App() {
     setCurrentIndex((currentIndex + 1) % cards.length);
     setIsFlipped(false);
   };
+
+  const handlePrevCard = () => {
+    if ((currentIndex - 1) < 0){
+      setCurrentIndex(cards.length - 1);
+    }
+    else{
+    setCurrentIndex((currentIndex - 1) % cards.length);
+    }
+    setIsFlipped(false);
+  }
+
+  const shuffleCards = () => {
+    const shuffled = [...cards];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const newCardIndex = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[newCardIndex]] = [shuffled[newCardIndex], shuffled[i]];
+    }
+    setCards(shuffled);
+    setCurrentIndex(0);
+    setIsFlipped(false);
+  }
 
   const currentCard = cards[currentIndex];
 
@@ -45,7 +65,9 @@ function App() {
         onClick={handleFlip}
       />
 
-      <button onClick={handleNextCard}>Next Card</button>
+      <button onClick={handlePrevCard}>Prev</button>
+      <button onClick={handleNextCard}>Next</button>
+      <button onClick={shuffleCards}>Shuffle Cards</button>
 
       {/* <img src={gif} title='Video Walkthrough' width='' alt='Video Walkthrough' /> */}
     </div>
